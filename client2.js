96286867d45c7c643a8b55d65c1f87474f8c5cc9
LeonardoCGoulart <joedarkalone@hotmail.com>
2021-12-09 15:41:57 -0300

    #commit 5 - client2.js

--- client2.js
+++ client2.js
@@ -1,1 +1,14 @@
-// oi eu sou o cliente 2
+const { default: axios } = require("axios");
+
+//visualiza a noticia
+axios.get('http://localhost:3000/noticia')
+.then((res) => {
+    console.log(res.data); // mostra todas as noticias existentes
+    axios.put('http://localhost:3000/enviar/'+res.data[0].ID+'') // capta uma e dispara os emails
+    .then(() => {
+        console.log("noticia 1 enviada com sucesso para todos os emails"); // sucesso
+    })
+})
+.catch(err => {
+    console.log(err.response.data);
+});
